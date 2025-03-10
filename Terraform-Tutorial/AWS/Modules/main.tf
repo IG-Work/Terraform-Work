99918e0a4c1934eedb0e2ed2c62ecd89e0187a7e
Yashi Chauhan <64953869+yashi-coder@users.noreply.github.com>
2025-03-10 14:33:08 +0530

    Update main.tf

--- Terraform-Tutorial/AWS/Modules/main.tf
+++ Terraform-Tutorial/AWS/Modules/main.tf
@@ -1,5 +1,10 @@
 
-module "vpc" {
+module "vpc_east" {
   source    = "./modules/vpc"
   cidr_block = "10.0.0.0/16"
+}
+
+module "vpc_west" {
+  source    = "./modules/vpc"
+  cidr_block = "10.1.0.0/16"
 }
