45d2a450fd863b98210705831dd2235c14ab99d8
Yashi Chauhan <yashi.chauhan@yashi.chauhan-GPGPW29HMX>
2025-03-10 11:22:21 +0530

    updated work

--- Terraform-Tutorial/HCP-Terraform/azure-cli-workspace/main.tf
+++ Terraform-Tutorial/HCP-Terraform/azure-cli-workspace/main.tf
@@ -3,10 +3,6 @@
 
 provider "azurerm" {
   features {}
-  /*client_id = "4e4ac9d4-20ec-4fb3-aa40-52720b583174"
-  client_secret= "1hh8Q~q~2o.WFJo9DgkAMIGy7OtU9B7nzODOPcd9"
-  tenant_id = "237fbc04-c52a-458b-af97-eaf7157c0cd4"
-  subscription_id="b8e2596b-e946-44ac-a26d-b40cbda624ef"*/
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
